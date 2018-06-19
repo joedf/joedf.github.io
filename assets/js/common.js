db3527491ed9164288ba7d6574922f3c5dc51382
Joe DF <joedf@live.ca>
2018-06-19 10:49:49 -0400

    CSS fixes and improvements

---
+++ assets/js/common.js
@@ -0,0 +1,22 @@
+// https://stackoverflow.com/a/950146/883015
+function loadScript(url, callback)
+{
+    // Adding the script tag to the head as suggested before
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = url;
+
+    // Then bind the event to the callback function.
+    // There are several events for cross browser compatibility.
+    script.onreadystatechange = callback;
+    script.onload = callback;
+
+    // Fire the loading
+    head.appendChild(script);
+}
+
+loadScript('assets/js/konami/konami.js', function(){
+	var cheatcode = new Konami(function() { alert('Konami Code!')});
+});
+
