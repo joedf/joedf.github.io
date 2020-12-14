77b03f724978eabe0ab78c5e62c5c0c3ac639926
joedf <joedf@live.ca>
2020-12-14 15:24:35 -0500

    Update dayandnight.js

--- assets/js/dayandnight.js
+++ assets/js/dayandnight.js
@@ -19,18 +19,6 @@ function getCookie(cname) {
     return "";
 }
 
-function checkCookie() {
-    var user = getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
-    } else {
-        user = prompt("Please enter your name:", "");
-        if (user != "" && user != null) {
-            setCookie("username", user, 365);
-        }
-    }
-}
-
 /////////----END---- cookie.js -----///////
 
 function DayAndNight(autorun) {
